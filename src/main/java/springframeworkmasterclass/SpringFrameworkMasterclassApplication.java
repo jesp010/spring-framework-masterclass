323c4b0d427b4806eaa1459d105a228aaa713d3a
jesp010 <juansolisosx@gmail.com>
2021-07-20 23:17:22 -0700

    first commit

---
+++ src/main/java/springframeworkmasterclass/SpringFrameworkMasterclassApplication.java
@@ -0,0 +1,13 @@
+package springframeworkmasterclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkMasterclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkMasterclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springframeworkmasterclass/SpringFrameworkMasterclassApplicationTests.java
@@ -0,0 +1,13 @@
+package springframeworkmasterclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkMasterclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
